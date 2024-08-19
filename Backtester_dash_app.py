09b1887ef1e4da5ed0581263bff1ba915e3c0c65
Felix Obioma Nkwuzor <felixobioma99@gmail.com>
2024-08-19 15:46:03 +0100

    Update Backtester_dash_app.py

--- Backtester_dash_app.py
+++ Backtester_dash_app.py
@@ -19,7 +19,7 @@ app = Dash(__name__,
           title='Built by Flexxie', 
           meta_tags=[{'name': 'viewport',
                       'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.5, minimum-scale=0.5'}],)
-server = app.server
+
 
 # In[3]:
 
