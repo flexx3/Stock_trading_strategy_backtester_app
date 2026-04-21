baf716404cc902ffcb49003824af02e15070fc20
Felix Obioma Nkwuzor <felixobioma99@gmail.com>
2026-04-21 15:08:26 +0100

    Update strategy_selector.py
    updated the wrangle function to use the newly created duckdb database engine

--- Backtester_web_app/app/strategy_selector.py
+++ Backtester_web_app/app/strategy_selector.py
@@ -21,59 +21,48 @@ from data import stock_data_api, Db_Repo
 load_dotenv()
 
 #general function to get data
-def wrangle(ticker, start_date, end_date):
-    #create the url object
-    url_object= URL.create(
-    "postgresql+psycopg", username=os.environ.get('DB_USERNAME'),
-    password=os.environ.get('DB_PASSWORD'), host=os.environ.get('DB_HOST'),
-    port=os.environ.get('DB_PORT'), database=os.environ.get('DB_NAME'))
-    #instantiate sqlalchemy engine
-    engine= create_engine(url_object)
-    #connect to database
-    with engine.connect() as conn:
-        #query to check if table exists
-        query= f''' SELECT EXISTS(
-        SELECT 1 FROM pg_tables
-        WHERE tablename = '{ticker}' AND schemaname = 'public');
-        '''
-        result = conn.execute(text(query))
-    table_exists= result.scalar()
-    #load data if table exists
-    if table_exists is True:
-        #instantiate 'dbrepo' class from data.py library
-        repo= Db_Repo()
-        #load data from table
-        df= repo.read_data(ticker)
-        #search through dataframe to get data between 'start_date' and 'end_date'
-        df= df.filter(pl.col('Date').is_between(datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d')))
-        #gets fresh data from api if specific range of data is not available from the database
-        if (df.is_empty()) or df['Date'].max() != (datetime.strptime(end_date, '%Y-%m-%d').date()):
-            api= stock_data_api(ticker)
-            data= api.get_data_from_api()
-            repo.insert_data(table_name=ticker, records=data)
+def wrangle(self, ticker, start_date, end_date):
+        #setup conection to db
+        engine= create_engine(f"duckdb:///{os.environ.get('DB_NAME')}")
+        with engine.connect() as conn:
+            #check if table exists
+            result= conn.execute(text(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{ticker}'"))
+            table_name= result.fetchone()
+        if table_name is not None:
+            #instantiate 'sqlrepo' class from data.py library
+            repo= Db_Repo(uri=f"duckdb:///{os.environ.get('DB_NAME')}")
             #load data from table
-            df= repo.read_data(ticker)
+            df= repo.read_table(ticker)
             #search through dataframe to get data between 'start_date' and 'end_date'
             df= df.filter(pl.col('Date').is_between(datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d')))
+            #gets fresh data from api if specific range of data is not available from the database
+            if (df.is_empty()) or df['Date'].max() != (datetime.strptime(end_date, '%Y-%m-%d').date()):
+                api= stock_data_api(ticker)
+                data= api.get_data_from_api()
+                repo.insert_data(table_name=ticker, records=data)
+                #load data from table
+                df= repo.read_table(ticker)
+                #search through dataframe to get data between 'start_date' and 'end_date'
+                df= df.filter(pl.col('Date').is_between(datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d')))
+            else:
+                df= df
         else:
-            df= df
-    else:
-        #instantiate 'api_data' class from data.py library
-        api= stock_data_api(ticker)
-        data= api.get_data_from_api()
-        #instantiate 'dbrepo' class from data.py library
-        repo= Db_Repo()
-        #setup connection to execute and commit changes to the db based on the below query
-        with engine.connect() as conn:
-            conn.execute(text(f'Drop Table If Exists "{ticker}"'))
-            conn.commit()
-        #insert data into database
-        repo.insert_data(table_name=ticker, records=data)
-        #load data from table
-        df=repo.read_data(ticker)
-        #search through dataframe to get data between 'start_date' and 'end_date'
-        df= df.filter(pl.col('Date').is_between(datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d')))
-    return df
+            #instantiate 'api_data' class from data.py library
+            api= stock_data_api(ticker)
+            data= api.get_data_from_api()
+            #instantiate 'sqlrepo' class from data.py library
+            repo= Db_Repo(uri=f"duckdb:///{os.environ.get('DB_NAME')}")
+            #setup connection to execute and commit changes to the db based on the below query
+            with engine.connect() as conn:
+                conn.execute(text(f'Drop Table If Exists "{ticker}"'))
+                conn.commit()
+            #insert data into database
+            repo.insert_data(table_name=ticker, records=data)
+            #load data from table
+            df=repo.read_table(ticker)
+            #search through dataframe to get data between 'start_date' and 'end_date'
+            df= df.filter(pl.col('Date').is_between(datetime.strptime(start_date, '%Y-%m-%d'), datetime.strptime(end_date, '%Y-%m-%d')))
+        return df
 
         
 def ema_rsi_strategy(ticker, start_date, end_date):
